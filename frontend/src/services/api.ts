a8a8ab3f7ba4c3ba5d61f2a913637193e392e079
Savas <savas.neto20@gmail.com>
2025-12-06 19:28:19 -0300

    urls changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,11 +1,16 @@
 import axios, { type AxiosResponse } from "axios";
-import { type User, type Book, type Notification, type PurchasePayload } from "../types";
+import {
+  type User,
+  type Book,
+  type Notification,
+  type PurchasePayload,
+} from "../types";
 
 const URLS = {
-  USERS: import.meta.env.VITE_API_USERS || "http://localhost:8081",
-  CATALOG: import.meta.env.VITE_API_CATALOG || "http://localhost:8084",
-  PAYMENT: import.meta.env.VITE_API_PAYMENT || "http://localhost:8082",
-  NOTIFICATION: import.meta.env.VITE_API_NOTIFICATION || "http://localhost:8083",
+  USERS: "https://ms-usuarios-kcc5.onrender.com",
+  CATALOG: "https://ms-catalogo-kcc5.onrender.com",
+  PAYMENT: "https://ms-pagamento-kcc5.onrender.com",
+  NOTIFICATION: "https://ms-notificacao-kcc5.onrender.com",
 };
 
 export const api = {
@@ -27,6 +32,8 @@ export const api = {
     axios.post(`${URLS.CATALOG}/compras`, data),
 
   // Notificações
-  getUserNotifications: (userId: number): Promise<AxiosResponse<Notification[]>> =>
+  getUserNotifications: (
+    userId: number
+  ): Promise<AxiosResponse<Notification[]>> =>
     axios.get(`${URLS.NOTIFICATION}/notificacoes/usuario/${userId}`),
 };
